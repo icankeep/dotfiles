ff00c8d2fa9cf31369383c547344c027d07fef3b
qzchenwl <qzchenwl@gmail.com>
2017-11-18 00:19:34 +0800

    update

--- misc/services/sites/haproxy/run.sh
+++ misc/services/sites/haproxy/run.sh
@@ -5,6 +5,6 @@ docker run -it --rm --name haproxy \
     -p 1936:1936 \
     -p 443:443 \
     -p 80:80 \
-    -v $PWD/ssl:/ssl \
-    -v $PWD/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+    -v "$HOME/Dropbox/etc/letsencrypt/live:/ssl:ro" \
+    -v "$PWD/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro" \
     haproxy
